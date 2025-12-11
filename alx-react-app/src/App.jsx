a41ba0e5c6ce665bf9c6992053755fa1cacbc4a0
Ogunbodede David <101398725+DavdePianist@users.noreply.github.com>
2025-12-11 23:11:27 +0100

    update

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
 
   return (
     <>
+    <welcome/>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
