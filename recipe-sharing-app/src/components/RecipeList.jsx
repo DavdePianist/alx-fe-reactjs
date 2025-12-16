c869df7fe917c5574948cf260937489abd83c5a7
Ogunbodede David <101398725+DavdePianist@users.noreply.github.com>
2025-12-16 14:51:53 +0100

    update and added new file

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,18 +1,20 @@
 import { useRecipeStore } from '../store/recipeStore';
+import { Link } from 'react-router-dom';
 
-function RecipeList() {
+const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
 
   return (
     <div>
+      <h2>Recipes</h2>
       {recipes.map((recipe) => (
         <div key={recipe.id}>
           <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
+          <Link to={`/recipes/${recipe.id}`}>View Details</Link>
         </div>
       ))}
     </div>
   );
-}
+};
 
 export default RecipeList;
