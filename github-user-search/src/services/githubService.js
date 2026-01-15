e70970fe5d9545fd3ffce89ba55f6145457fbbbc
Ogunbodede David <101398725+DavdePianist@users.noreply.github.com>
2026-01-15 15:01:57 +0100

    update github

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -3,10 +3,21 @@ import axios from "axios"
 // GitHub Search Users API
 const BASE_URL = "https://api.github.com/search/users?q="
 
-// ✅ async + await + explicit URL
-export const searchUsers = async (query) => {
+// ✅ includes location and minRepos explicitly
+export const searchUsers = async (query, location, minRepos) => {
+  let searchQuery = query
+
+  if (location) {
+    searchQuery += ` location:${location}`
+  }
+
+  if (minRepos) {
+    searchQuery += ` repos:>=${minRepos}`
+  }
+
   const response = await axios.get(
-    `${BASE_URL}${query}`
+    `${BASE_URL}${searchQuery}`
   )
+
   return response.data
 }
