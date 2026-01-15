971d90dbd0d6c1cb983d84c7423fb470eacaf10b
Ogunbodede David <101398725+DavdePianist@users.noreply.github.com>
2026-01-15 14:51:09 +0100

    update

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,60 +1,11 @@
-import { useState } from "react";
-import Search from "./components/Search";
-import { fetchUserData } from "./services/githubService";
-
 function App() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-
-  const handleSearch = async (filters) => {
-    setLoading(true);
-    setError("");
-
-    try {
-      const data = await fetchUserData(filters);
-      setUsers(data.items);
-    } catch (err) {
-      setError("Looks like we can’t find the user");
-    } finally {
-      setLoading(false);
-    }
-  };
-
   return (
-    <div className="min-h-screen bg-gray-100 p-6">
-      <Search onSearch={handleSearch} />
-
-      {loading && <p className="text-center mt-4">Loading...</p>}
-      {error && <p className="text-center mt-4 text-red-500">{error}</p>}
-
-      <div className="grid gap-4 mt-6 max-w-xl mx-auto">
-        {users.map((user) => (
-          <div
-            key={user.id}
-            className="bg-white p-4 rounded-lg shadow flex items-center gap-4"
-          >
-            <img
-              src={user.avatar_url}
-              alt={user.login}
-              className="w-16 h-16 rounded-full"
-            />
-
-            <div>
-              <h3 className="font-semibold">{user.login}</h3>
-              <a
-                href={user.html_url}
-                target="_blank"
-                className="text-blue-600 text-sm"
-              >
-                View Profile
-              </a>
-            </div>
-          </div>
-        ))}
-      </div>
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-5xl font-bold text-emerald-400">
+        Tailwind FIXED 🎉
+      </h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
